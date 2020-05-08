98e8d028e7ce75867597937ad62c9eb1600cfa0e
github username <github email address>
2020-05-07 20:41:06 -0400

    form is proving hard

--- my-project-client/src/PostcardForm.js
+++ my-project-client/src/PostcardForm.js
@@ -2,20 +2,57 @@ import React, { Component } from 'react'
 import { Button, Checkbox, Form } from 'semantic-ui-react'
 
 class PostcardForm extends Component {
+    state = {
+        image: null,
+        text: null
+    }
 
+    onSubmit = (e) => {
+        e.preventDefault()
+        console.log()
+        //clears form data for next entry
+        this.setState({
+            image: null,
+            text: null
+        })
 
+        fetch("http://localhost:3000/postcards", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: {
+                "image": this.state.image,
+                "text": this.state.text
+            }
+        })
+            .then(r => r.json())
+            .then(data => console.log(data))
+
+    }
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+        console.log(this.state)
+    }
+
+    //submit button doesn't work arrrgh!
     render() {
         return (
             <Form>
-                <Form.Input label='Enter Password' type='image' />
                 <Form.Field>
-                    <label>Last Name</label>
-                    <input placeholder='Last Name' />
+                    <label>Select An Image from Your Computer</label>
+                    <input type="file" onChange={e => this.onChange(e)} name="image" value={this.state.image} />
                 </Form.Field>
                 <Form.Field>
-                    <Checkbox label='I agree to the Terms and Conditions' />
+                    <label>Text</label>
+                    <input name="text" onChange={e => this.onChange(e)} value={this.state.text} placeholder='Add Some Text' />
                 </Form.Field>
-                <Button type='submit'>Submit</Button>
+                <Form.Field>
+                    <Checkbox label='I forgot why I wanted a checkbox here' />
+                </Form.Field>
+                <Button onSubmit={e => this.onSubmit(e)} type='submit'>Submit</Button>
             </Form>
         )
     }
